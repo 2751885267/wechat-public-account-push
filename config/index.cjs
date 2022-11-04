2255f02866b2f7b1d652414ea9f2eecc1f01934f
2751885267 <117375828+2751885267@users.noreply.github.com>
2022-11-04 11:14:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -66,7 +66,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-11-15' },
+        { keyword: 'love_day', date: '2022-11-05' },
       ],
     },
   ],
